76227610abed89aaecc900243b7a80437f5c0f26
WongQi <wongql-wp16@student.tarc.edu.my>
2019-12-08 17:19:49 +0800

    fixed reset

--- app/src/main/java/com/android/example/lab1/MainActivity.kt
+++ app/src/main/java/com/android/example/lab1/MainActivity.kt
@@ -8,24 +8,43 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textPrice: TextView
+    lateinit var  textDown :TextView
+    lateinit var textInterestRate :TextView
+    lateinit var textPeriod:TextView
+    lateinit var textLoan:TextView
+    lateinit var textInterest:TextView
+    lateinit var  textMonthly:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val calculateButton: Button = findViewById(R.id.buttonCalculate)
-
+        val resetButton :Button =findViewById(R.id.buttonReset)
+        resetButton.setOnClickListener{reset()}
 
         calculateButton.setOnClickListener{calculate()}
+         textPrice = findViewById(R.id.editTextCarPrice)
+        textDown= findViewById( R.id.editTextDownPayment)
+        textInterestRate = findViewById(R.id.editTextInterestRate)
+        textPeriod  = findViewById(R.id.editTextLoanPeriod)
+         textLoan = findViewById(R.id.textViewLoan)
+        textInterest = findViewById(R.id.textViewInterest)
+        textMonthly = findViewById(R.id.textViewMonthlyRepayment)
+    }
+
+    private fun reset() {
+        textPrice.text=null
+        textDown.text=null
+        textInterestRate.text= null
+        textPeriod.text = null
+        textLoan.text = getString(R.string.loan)
+        textInterest.text = getString(R.string.interest)
+        textMonthly.text = getString(R.string.monthly_repayment)
     }
 
     private fun calculate(){
-        val textPrice : TextView = findViewById(R.id.editTextCarPrice)
-        val textDown : TextView = findViewById( R.id.editTextDownPayment)
-        val textInterestRate :  TextView = findViewById(R.id.editTextInterestRate)
-        val textPeriod :TextView = findViewById(R.id.editTextLoanPeriod)
-        val textLoan :TextView = findViewById(R.id.textViewLoan)
-        val textInterest : TextView = findViewById(R.id.textViewInterest)
-        val textMonthly :TextView = findViewById(R.id.textViewMonthlyRepayment)
+
 
        val price: Double = textPrice.text.toString().toDouble()
         val downPayment : Double =textDown.text.toString().toDouble()
